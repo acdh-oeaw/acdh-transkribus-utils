66de43326e239471031dc2d3b6487db94bc2b67c
csae8092 <peter.andorfer@oeaw.ac.at>
2023-03-01 17:17:57 +0100

    first proper tests #2

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,5 +1,11 @@
 import unittest
 
+from transkribus_utils import ACDHTranskribusUtils
+
+CLIENT = ACDHTranskribusUtils()
+COL_NAME = "bv-play"
+COL_ID = 188933
+
 
 class TestTestTest(unittest.TestCase):
     """Tests for `acdh_cidoc_pyutils` package."""
@@ -12,3 +18,12 @@ class TestTestTest(unittest.TestCase):
 
     def test_001_smoke(self):
         self.assertEqual(1, 1)
+
+    def test_002_init(self):
+        client = CLIENT
+        self.assertTrue("http" in client.base_url)
+
+    def test_003_get_or_create_col(self):
+        client = CLIENT
+        col = client.get_or_create_collection(COL_NAME)
+        self.assertEqual(col, COL_ID)
