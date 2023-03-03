3d484a6f2647d425d9c318bb174a695c7e40764d
csae8092 <peter.andorfer@oeaw.ac.at>
2023-03-03 12:46:51 +0100

    version bump 2.3 -> 2.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as readme_file:
 
 setup(
     name="acdh-transkribus-utils",
-    version="2.3",
+    version="2.4",
     description="""some utility function to interact with the Transkribus-API""",
     long_description=readme,
     long_description_content_type="text/markdown",
