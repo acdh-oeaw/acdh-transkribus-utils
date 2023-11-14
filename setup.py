06d831ed5fd641a7db8932de348a5ea4279442ad
csae8092 <peter.andorfer@oeaw.ac.at>
2023-11-14 16:10:57 +0100

    removed outdated things from setup.py

--- setup.py
+++ setup.py
@@ -26,15 +26,4 @@ setup(
     license="MIT",
     zip_safe=False,
     keywords="acdh-transkribus-utils",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Framework :: Django :: 2.0",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-    ],
 )
